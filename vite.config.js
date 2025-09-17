66366941f75fd5a0c2a1592c0eee6d0a81fe436f
ClementBerthier <berthierclementpro@gmail.com>
2025-09-17 19:40:54 +0200

    vit config new

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        {
-            name: "hint-modulepreload",
-            apply: "build",
-            enforce: "post",
-            transformIndexHtml(html, ctx) {
-                if (!ctx?.bundle) return html;
-                const tags = [];
-                for (const [, chunk] of Object.entries(ctx.bundle)) {
-                    if (chunk.type === "chunk" && chunk.isEntry) {
-                        tags.push({
-                            tag: "link",
-                            attrs: {
-                                rel: "modulepreload",
-                                href: `/${chunk.fileName}`,
-                            },
-                            injectTo: "head",
-                        });
-                    }
-                }
-                return { html, tags };
-            },
-        },
-    ],
-    build: {
-        modulePreload: { polyfill: true }, // au cas où
-        sourcemap: false,
-        minify: "esbuild",
-        target: "es2018",
-    },
+    plugins: [react()],
 });
