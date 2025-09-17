8eb67cd309db15e288ad28c04dfafe5762e6893d
ClementBerthier <berthierclementpro@gmail.com>
2025-09-17 19:00:47 +0200

    add code on vite config

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        {
-            name: "inline-all-css",
-            apply: "build",
-            enforce: "post",
-            transformIndexHtml(html, ctx) {
-                // En build seulement, on a accès au bundle Rollup
-                if (!ctx?.bundle) return html;
-
-                // Concatène tout le CSS émis par Vite
-                let css = "";
-                for (const [, asset] of Object.entries(ctx.bundle)) {
-                    if (
-                        asset.type === "asset" &&
-                        asset.fileName.endsWith(".css")
-                    ) {
-                        css += String(asset.source || "");
-                        // optionnel: on retire le fichier CSS du bundle
-                        delete ctx.bundle[asset.fileName];
-                    }
-                }
-                if (!css) return html;
-
-                // Supprime tous les <link rel="stylesheet"> et injecte le <style>
-                const cleaned = html.replace(
-                    /<link[^>]+rel=["']stylesheet["'][^>]*>/g,
-                    ""
-                );
-                return {
-                    html: cleaned,
-                    tags: [{ tag: "style", children: css, injectTo: "head" }],
-                };
-            },
-        },
-    ],
+    plugins: [react()],
 });
