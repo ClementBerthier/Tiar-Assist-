9202b4864ea46e8d07e48eb4c0155897878722b4
ClementBerthier <berthierclementpro@gmail.com>
2025-09-18 09:57:49 +0200

    lazy loadin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,23 @@
 import Home from "../Home/Home";
 import Services from "../Services/Services";
 import Contact from "../Contact/Contact";
+import { lazy, Suspense } from "react";
 
 import "../../styles/App.css";
 import Footer from "../Footer/Footer.jsx";
 
 function App() {
+    const Services = lazy(() => import("../Services/Services"));
+    const Contact = lazy(() => import("../Contact/Contact"));
+    const Footer = lazy(() => import("../Footer/Footer"));
     return (
         <>
             <Home />
-            <Services />
-            <Contact />
-            <Footer />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Services />
+                <Contact />
+                <Footer />
+            </Suspense>
         </>
     );
 }
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import critters from "vite-plugin-critters";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        critters({
-            preload: "swap", // transforme <link rel="stylesheet"> en non-bloquant
-            reduceInlineStyles: true,
-        }),
-    ],
+    plugins: [react()],
 });
