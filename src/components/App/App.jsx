ef2224d89b9ef7c6bc21a6ed9aabf63a34c013ba
ClementBerthier <berthierclementpro@gmail.com>
2025-09-18 10:44:13 +0200

    test lazy utils

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,17 @@
 import Home from "../Home/Home";
 import Services from "../Services/Services";
-import { lazy, Suspense } from "react";
+import Contact from "../Contact/Contact";
 
 import "../../styles/App.css";
+import Footer from "../Footer/Footer.jsx";
 
 function App() {
-    const Contact = lazy(() => import("../Contact/Contact"));
-    const Footer = lazy(() => import("../Footer/Footer"));
     return (
         <>
             <Home />
             <Services />
-            <Suspense fallback={<div>Loading...</div>}>
-                <Contact />
-                <Footer />
-            </Suspense>
+            <Contact />
+            <Footer />
         </>
     );
 }
