7e30c933ab17226d6b6aee40dfb3044409ed66b8
ClementBerthier <berthierclementpro@gmail.com>
2025-09-18 10:40:52 +0200

    test lazy utils

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,22 @@
 import Home from "../Home/Home";
-import Services from "../Services/Services"; // si visible au-dessus du pli, NE PAS lazy-load
-import LazyWhenVisible from "../../utils/LazyWhenVisible.jsx";
+import Services from "../Services/Services";
+import { lazy, Suspense } from "react";
 
-export default function App() {
+import "../../styles/App.css";
+
+function App() {
+    const Contact = lazy(() => import("../Contact/Contact"));
+    const Footer = lazy(() => import("../Footer/Footer"));
     return (
         <>
             <Home />
             <Services />
-
-            {/* Contact seulement quand visible */}
-            <LazyWhenVisible
-                importFn={() => import("../Contact/Contact")} // <— import dynamique uniquement à l’intersection
-                fallback={<div style={{ minHeight: 320 }} aria-hidden="true" />}
-                rootMargin="0px" // pas de chargement anticipé
-                threshold={0.01}
-            />
-
-            {/* Footer encore plus tard */}
-            <LazyWhenVisible
-                importFn={() => import("../Footer/Footer")}
-                fallback={<div style={{ minHeight: 200 }} aria-hidden="true" />}
-                rootMargin="0px"
-                threshold={0.01}
-            />
+            <Suspense fallback={<div>Loading...</div>}>
+                <Contact />
+                <Footer />
+            </Suspense>
         </>
     );
 }
+
+export default App;
